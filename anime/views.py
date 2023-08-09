d6f6c08eaddfdd3ddd0551dfe006d015b33949c7
Shayan <shayanghodos@gmail.com>
2023-08-09 11:12:16 +0330

    AririSan

--- anime/views.py
+++ anime/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import status
+from anime.pagination import DefaultPagination
 from .serializers import AnimeSerializer, ListAnimeSerializer, ListAnimeItmeSerializer, CommentSerializer, AddListAnimeItemSerializer, PostCommentSerializer, UpdateCommentSerializer
 from .models import Anime, ListAnime, ListAnimeItem, Comment
 from .permissions import IsAdminOrReadOnly
@@ -11,6 +17,11 @@ class AnimeViewSet(ModelViewSet):
     serializer_class = AnimeSerializer
     queryset = Anime.objects.prefetch_related('comments').all()
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    pagination_class = DefaultPagination
+    permission_classes = [IsAdminOrReadOnly]
+    search_fields = ['name', 'summery']
+    ordering_fields = ['name', 'myanimelist_score', 'released_date']
 
     
 class ListAnimeViewSet(ModelViewSet):
@@ -61,6 +72,14 @@ class CommentViewSet(ModelViewSet):
         user_id = self.request.user.id
         anime_id = self.kwargs['anime_pk']
         return {'user_id':user_id, 'anime_id':anime_id}
+    
+    def destroy(self, request, *args, **kwargs):
+        user_id = self.request.user.id
+        comment = Comment.objects.get(id=kwargs['pk'])
+        if user_id != comment.user.id:
+            return Response({'error': 'Comment can not delete.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
     
 
 
