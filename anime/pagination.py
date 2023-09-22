d77c10e72c2761b5a93f1aea65ab3b5cb99a62e3
Shayan <shayanghodos@gmail.com>
2023-09-22 22:26:11 +0330

    Ariri_San

--- anime/pagination.py
+++ anime/pagination.py
@@ -3,12 +3,18 @@ from rest_framework.response import Response
 from collections import OrderedDict
 
 class DefaultPagination(PageNumberPagination):
+  # template = 
+
   def get_paginated_response(self, data):
     return Response(OrderedDict([
-            ('count', self.page.paginator.count),
-            ('html_page', self.get_html_context),
-            ('next', self.get_next_link()),
-            ('previous', self.get_previous_link()),
-            ('results', data)
-        ]))
+      ('count', self.page.paginator.count),
+      ('html_page', self.get_html_context()),
+      ('next', self.get_next_link()),
+      ('previous', self.get_previous_link()),
+      ('results', data)
+    ]))
+
+
   page_size = 50
+
+  
